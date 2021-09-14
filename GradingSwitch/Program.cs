2515b4948e4c2c68096d5791feadc0c8b10e7a0e
Opilane TTHK <opilane@tthk.ee>
2021-09-14 13:53:21 +0300

    first commit

---
+++ GradingSwitch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GradingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mis hinnet sa said?");
+            string UserGrade = Console.ReadLine();ToLower();
+
+            switch (UserGrade)
+            {
+                case "5":
+                    Console.WriteLine("Suurepärane");
+                    break;
+                case "4":
+                    Console.WriteLine("Väga hea");
+                    break;
+                case "3":
+                    Console.WriteLine("Hea");
+                    break;
+                case "2":
+                    Console.WriteLine("Rahuldav");
+                    break;
+                case "1":
+                    Console.WriteLine("Puudulik");
+                    break;
+                default:
+                    Console.WriteLine("Puudulik");
+                    break;
+
+            
+           
+            
+             
+            }
+            Console.WriteLine("Kena päeva!");
+        }
+
+    }
+}
